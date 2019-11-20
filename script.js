677052ab206c2f80815ed4aa7f4af6961959f413
Paweł Wolski <pawelwolski3@gmail.com>
2019-11-20 11:42:51 +0100

    finish

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const panels = document.querySelectorAll('.panel')
 
-function openImage (e) {
-e.target.classList.add('open')
+function openImage(e) {
+
+  this.classList.toggle('open')
 }
 
-panels.forEach((panel)=> panel.addEventListener('click', openImage))
+function showWords(e) {
+ 
+  if (e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
+
+panels.forEach((panel) => panel.addEventListener('click', openImage))
+
+panels.forEach((panel) => panel.addEventListener('transitionend', showWords))
+
+
